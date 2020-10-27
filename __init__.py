8ab7906217e2eb6ecfffe1d65f0fc189742b1923
aachhabra <aachhabra@SEA.CORP.EXPECN.COM>
2020-10-27 11:24:33 +0530

    Added Is Category

--- __init__.py
+++ __init__.py
@@ -1,10 +1,41 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+
 app = Flask(__name__)
-@app.route('/')
-def home():
-    return render_template("index.html")
-@app.route('/courses')
-def courses():
-    return render_template("courses.html")
+
+class ReusableForm(Form):
+	ques = TextField("Question:",validators=[validators.required()])
+
+	@app.route('/')
+	def home():
+	    return render_template("index.html")
+
+	@app.route('/courses')
+	def courses():
+		select = request.form.get('myfield')
+		print(str(select)) # just to see what select is
+		return render_template("courses.html")
+
+	@app.route('/is',methods=['GET', 'POST'])
+	def in_ques():
+		out = ""
+		temp = ""
+		form = ReusableForm(request.form)
+
+		if(request.method=="POST"):
+			if("pesticides" in request.form['name']):
+				temp = "What is the form of pesticides"
+			else:
+				temp = "What is your mood right now?"
+			print(request.form['name'])
+
+		if(form.validate()):
+			print("Done")
+		else:
+			print("Not Done")
+
+		out=temp
+		return render_template("is.html",form=form,out = out)
+
 if __name__ == '__main__':
     app.run(debug=True)
